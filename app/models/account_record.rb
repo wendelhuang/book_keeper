e8dae4ffb50b7bc3b0765f6125801a4429f73357
Huangweiwei <448462116@qq.com>
2015-12-10 21:29:49 +0800

    MOD: add validate & scope

--- app/models/account_record.rb
+++ app/models/account_record.rb
@@ -1,2 +1,9 @@
 class AccountRecord < ActiveRecord::Base
+  validates_presence_of :record_type, :amounts
+  with_options :allow_blank => true do |v|
+    v.validates_numericality_of :amounts
+  end
+
+  scope :incomings => lambda { where("#{AccountRecord.table_name}.incoming_or_outgoing = 1") }
+  scope :outgoings => lambda { where("#{AccountRecord.table_name}.incoming_or_outgoing = -1") }
 end
