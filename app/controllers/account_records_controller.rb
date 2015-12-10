01372ae7ef963ebddda7d6b983546d04a0832cac
Huangweiwei <448462116@qq.com>
2015-12-10 21:31:02 +0800

    MOD: extract code to before_filter

--- app/controllers/account_records_controller.rb
+++ app/controllers/account_records_controller.rb
@@ -1,19 +1,26 @@
 class AccountRecordsController < ApplicationController
+
+  before_filter :set_time, :only => [:new, :create]
+
   def index
     @account_records = AccountRecord.all
-    @incoming_records = @account_records.select {|account_record| account_record.incoming_or_outgoing == 1 }
-    @outgoing_records = @account_records.select {|account_record| account_record.incoming_or_outgoing == -1 }
+    #@incoming_records = @account_records.select {|account_record| account_record.incoming_or_outgoing == 1 }
+    #@outgoing_records = @account_records.select {|account_record| account_record.incoming_or_outgoing == -1 }
+    @incoming_records = AccountRecord.incomings
+    @outgoing_records = AccountRecord.outgoings
   end
 
   def new
     @account_record = AccountRecord.new
-    @time = Time.new
   end
 
   def create
     @account_record = AccountRecord.new(account_record_params)
-    @account_record.save
-    redirect_to account_records_path
+    if @account_record.save
+      redirect_to account_records_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -25,5 +32,8 @@ class AccountRecordsController < ApplicationController
   private
   def account_record_params
     params.require(:account_record).permit(:amounts, :incoming_or_outgoing, :record_type, :description)
+  end
+  def set_time
+    @time = Time.new
   end
 end
