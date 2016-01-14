291cb2c9fc30c45603e956e959797f25499ffc1e
Huangweiwei <448462116@qq.com>
2016-01-14 21:30:46 +0800

    BUGFIX: bootstrap class error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def flash_bootstrap_class(level)
-    case level
-    when :notice then "alert alert-info"
+    case level.to_sym
     when :success then "alert alert-success"
-    when :error then "alert alert-error"
-    when :alert then "alert alert-error"
+    when :notice then "alert alert-info"
+    when :alert then "alert alert-danger"
+    when :error then "alert alert-danger"
     end
   end
 end
