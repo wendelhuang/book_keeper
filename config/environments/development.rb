2a47bcf204828b9dc1a56338b55fb820f3b83789
Huangweiwei <448462116@qq.com>
2016-03-10 07:55:49 +0800

    MOD: config paperclip's command opinion

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.log_level = :info
+
+  # paperclip config
+  Paperclip.options[:command_path] = '/usr/bin'
 end
